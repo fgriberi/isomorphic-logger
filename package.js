14f957a4011351e2e5dc5987431b84a2dcdfd4bd
Franco Riberi <fgriberi@gmail.com>
2017-08-04 01:00:54 -0300

    [master] remove package test - temporarly

--- package.js
+++ package.js
@@ -9,20 +9,11 @@ Package.describe({
 Npm.depends({
   'winston': '2.3.1',
   'winston-daily-rotate-file': '1.4.6',
-  'tinytest': '1.1.4'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.4');
-  api.use('ecmascript');
   api.mainModule('isomorphic-logger-client.js', 'client');
   api.mainModule('isomorphic-logger-server.js', 'server');
   api.mainModule('isomorphic-logger.js', 'server');
-});
-
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('ecmascript');
-  api.use('fgriberi:isomorphic-logger');
-  api.mainModule('isomorphic-logger-tests.js');
 });
